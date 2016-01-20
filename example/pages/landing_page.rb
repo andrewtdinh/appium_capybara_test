df1e2e775693016dde420a5a3350a4e941813228
Andrew Dinh <andrewdinh74@yahoo.com>
2016-01-20 10:27:32 -0800

    Able to locate an element and click on it

--- example/pages/landing_page.rb
+++ example/pages/landing_page.rb
@@ -4,19 +4,19 @@ module Pages
   class LandingPage < SitePrism::Page
     # include Capybara::DSL
     ###########   WELCOME OBJECTS   #############
-    element :signup_button, :accessibility_id, 'welcome_signup_button'
-    element :login_button, :accessibility_id, 'welcome_login_button'
+    element :signup_button, :id, 'welcome_signup_button'
+    element :login_button, :id, 'welcome_login_button'
     # LOGIN_BUTTON_LOCATOR          = {id: 'welcome_login_button'}
     # BROWSE_PROVIDERS_LINK_LOCATOR = {id: 'welcome_login_button_browse_provider_text_view'}
     # login_button = id('android:id'/welcome_login_button)
 
     #############  CLICK LOCATORS   #############
     def click_signup_button
-      click signup_button
+      signup_button.click
     end
 
     def click_login_button
-      click login_button
+      login_button.click
     end
 
     # def click_browse_providers_link
