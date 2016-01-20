b4864b1c961f02d56184d4de7ec7f5fc82e457f3
Andrew Dinh <andrewdinh74@yahoo.com>
2016-01-20 13:17:03 -0800

    Add more locators and methods for the landing page

--- example/pages/landing_page.rb
+++ example/pages/landing_page.rb
@@ -2,26 +2,27 @@
 
 module Pages
   class LandingPage < SitePrism::Page
-    # include Capybara::DSL
+    include Capybara::DSL
     ###########   WELCOME OBJECTS   #############
     element :signup_button, :id, 'welcome_signup_button'
     element :login_button, :id, 'welcome_login_button'
-    # LOGIN_BUTTON_LOCATOR          = {id: 'welcome_login_button'}
-    # BROWSE_PROVIDERS_LINK_LOCATOR = {id: 'welcome_login_button_browse_provider_text_view'}
-    # login_button = id('android:id'/welcome_login_button)
+    element :browse_providers_link, :id, 'welcome_login_button_browse_provider_text_view'
+    element :seeking_therapy_text, :id, 'welcome_seeking_therapy_text_view'
+
 
     #############  CLICK LOCATORS   #############
     def click_signup_button
       signup_button.click
+
     end
 
     def click_login_button
       login_button.click
     end
 
-    # def click_browse_providers_link
-    #   click_on BROWSE_PROVIDERS_LINK_LOCATOR
-    # end
+    def verify_page
+      expect(seeking_therapy_text).to be_truthy
+    end
   end
 end
 
--- example/spec/features/login/login_spec.rb
+++ example/spec/features/login/login_spec.rb
@@ -11,6 +11,7 @@ describe 'Android Test' do
 
   it 'should be able to login as ph+c1' do
     @landing_page.click_login_button
+
     # login.set_email_field('ph+c1@breakthrough.com')
     # login.set_password_field('asdfasdf')
     # login.click_login_button
